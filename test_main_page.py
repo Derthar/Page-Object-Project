afa76c03310bb6e897dc5555528060cc703b9c2e
Derthar <dertgar159@gmail.com>
2023-02-27 17:49:07 +0500

    Add first page

--- test_main_page.py
+++ test_main_page.py
@@ -2,10 +2,13 @@ import time
 
 from selenium.webdriver.common.by import By
 
+link = "http://selenium1py.pythonanywhere.com/"
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+def go_to_login_page(browser):
     browser.get(link)
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+
+def test_guest_can_go_to_login_page(browser):
+    go_to_login_page(browser)
     time.sleep(10)
