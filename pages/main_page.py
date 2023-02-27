4d9bae14d0c45e5cd310b7a020e874ba8c3ff8e6
Derthar <dertgar159@gmail.com>
2023-02-27 19:07:37 +0500

    Little improved first tests

--- pages/main_page.py
+++ pages/main_page.py
@@ -7,5 +7,8 @@ class MainPage(BasePage):
     def __init__(self, browser, link):
         super().__init__(browser, link)
 
+    def should_be_login_link(self):
+        assert self.browser.find_element(By.CSS_SELECTOR, "#login_link"), "Login link is not presented!"
+
     def go_to_login_page(self):
         self.browser.find_element(By.CSS_SELECTOR, "#login_link").click()
--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,7 @@
 import time
+
+import pytest
+
 from pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 
@@ -12,8 +15,15 @@ def go_to_login_page(browser):
     login_link.click()
 
 
+@pytest.mark.usefixtures
 def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    time.sleep(10)
+
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
     time.sleep(10)
